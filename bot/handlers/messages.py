fcd95a51bbfe1ec0184106c0b917c36da8377662
torrua <torrua@users.noreply.github.com>
2023-03-07 11:41:23 +0700

    Remove parse_mode

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -3,7 +3,7 @@
 Telegram bot messages functions
 """
 
-from bot import bot, DEFAULT_PARSE_MODE, msg, MESSAGE_NOT_FOUND, DEFAULT_LANGUAGE
+from bot import bot, msg, MESSAGE_NOT_FOUND, DEFAULT_LANGUAGE
 from config.model_telegram import TelegramWord as Word
 from app import Session
 
@@ -19,15 +19,15 @@ def bot_text_messages_handler(message: msg) -> None:
 
     if words := Word.by_request(Session, user_request):
         for word in words:
-            word.send_card_to_user(Session, bot, message.chat.id, DEFAULT_PARSE_MODE)
+            word.send_card_to_user(Session, bot, message.chat.id)
     elif translation := Word.translation_by_key(Session, user_request, DEFAULT_LANGUAGE):
         bot.send_message(
             chat_id=message.chat.id,
             text=translation,
-            parse_mode=DEFAULT_PARSE_MODE)
+        )
 
     else:
         bot.send_message(
             chat_id=message.chat.id,
             text=MESSAGE_NOT_FOUND % user_request,
-            parse_mode=DEFAULT_PARSE_MODE, )
+        )
