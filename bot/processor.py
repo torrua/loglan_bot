bc5393dd5d03950821613ef87243a9537e5e4517
torrua <torrua@users.noreply.github.com>
2023-03-08 07:07:36 +0700

    Remove app context from bot.processor

--- bot/processor.py
+++ bot/processor.py
@@ -7,7 +7,6 @@ from bot import bot, msg, cbq
 from bot.handlers.commands import bot_cmd_start, bot_cmd_gle, bot_cmd_log
 from bot.handlers.messages import bot_text_messages_handler
 from bot.handlers.inline import bot_callback_inline
-from app import app
 
 
 @bot.message_handler(commands=["start"])
@@ -17,41 +16,27 @@ def command_start(message: msg):
     :param message:
     :return:
     """
-    with app.app_context():
-        bot_cmd_start(message)
+    bot_cmd_start(message)
 
 
-@bot.message_handler(
-    commands=[
-        "g",
-        "e",
-        "gleci",
-    ]
-)
+@bot.message_handler(commands=["g", "e", "gleci", ])
 def command_gleci(message: msg):
     """
     Handle command /gleci
     :param message:
     :return:
     """
-    with app.app_context():
-        bot_cmd_gle(message)
+    bot_cmd_gle(message)
 
 
-@bot.message_handler(
-    commands=[
-        "l",
-        "logli",
-    ]
-)
+@bot.message_handler(commands=["l", "logli", ])
 def command_logli(message: msg):
     """
     Handle command /logli
     :param message:
     :return:
     """
-    with app.app_context():
-        bot_cmd_log(message)
+    bot_cmd_log(message)
 
 
 @bot.message_handler(regexp="/[a-z]+")
@@ -62,8 +47,7 @@ def cpx_messages_handler(message: msg):
     :param message:
     :return:
     """
-    with app.app_context():
-        bot_text_messages_handler(message)
+    bot_text_messages_handler(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -73,5 +57,4 @@ def callback_inline(call: cbq):
     :param call: Incoming inline request
     :return:
     """
-    with app.app_context():
-        bot_callback_inline(call)
+    bot_callback_inline(call)
