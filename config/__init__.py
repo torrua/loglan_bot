357a5051bd0f4be74043320f8cbf6e83a92dd7bc
torrua <torrua@users.noreply.github.com>
2023-03-13 14:11:44 +0700

    FIX  C0301: Line too long (106/100) (line-too-long)

--- config/__init__.py
+++ config/__init__.py
@@ -9,7 +9,9 @@ import os
 
 logging.basicConfig(
     # format='%(message)s',
-    format='%(filename)s [LINE:%(lineno)d]\t[%(asctime)s] %(levelname)-s\t%(funcName)s() \t\t%(message)s',
+    format='%(filename)s [LINE:%(lineno)d]'
+           '\t[%(asctime)s] %(levelname)-s'
+           '\t%(funcName)s() \t\t%(message)s',
     level=logging.DEBUG,
     datefmt="%y-%m-%d %H:%M:%S")
 
