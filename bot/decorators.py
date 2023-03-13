f97ecc4c7a26224f1bfa2c0967a61e393368dab7
torrua <torrua@users.noreply.github.com>
2023-03-13 10:57:22 +0700

    Update decorator's logging and add docstring

--- bot/decorators.py
+++ bot/decorators.py
@@ -1,22 +1,41 @@
+"""
+Decorators for Bot functions
+"""
+
 import time
 from functools import wraps
 from config import log
 
 
 def logging_time(func):
+    """
+    :param func:
+    :return:
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        def duration(start, end):
+            return f'{end - start:.2f}'
+
         start_time = time.time()
         log.debug(
-            f"{func.__name__} - Start time:"
-            f" {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
+            "%s - Start time: %s" %
+            (func.__name__, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)))
+        )
         result = func(*args, **kwargs)
         end_time = time.time()
         log.debug(
-            f"{func.__name__} - End time:"
-            f" {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
+            "%s - End time: %s" %
+            (func.__name__, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)))
+        )
         log.debug(
-            f"{func.__name__} - Duration:"
-            f" {end_time - start_time:.2f} seconds")
+            "%s - Duration: %s seconds" %
+            (func.__name__, duration(start_time, end_time))
+        )
         return result
     return wrapper
