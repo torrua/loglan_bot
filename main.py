efd9213ee440270e593d120ede3811bdbf571d0a
torrua <torrua@users.noreply.github.com>
2023-09-15 08:39:53 +0700

    fix app run

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """The main module for launching web application"""
 
-from app import app
+from app import create_app
 from app.logger import log
 
+app = create_app()
 log.debug(app.name)
