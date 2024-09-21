faf0914fd0f9df91d85ce35277657db7edf525f4
torrua <torrua@users.noreply.github.com>
2024-09-21 16:18:14 +0700

    Fix unique() request for bot

--- app/bot/telegram/handlers/messages.py
+++ app/bot/telegram/handlers/messages.py
@@ -20,8 +20,10 @@ async def bot_text_messages_handler(message: msg) -> None:
 
     user_request = message.text.removeprefix("/")
     with Session() as session:
-        words = WordSelector().by_name(user_request).with_relationships().all(session)
-
+        words_result = session.execute(
+            WordSelector().by_name(user_request).with_relationships().get_statement()
+        )
+        words = words_result.unique().scalars().all()
     if words:
         for word in words:
             await bot.send_message(
